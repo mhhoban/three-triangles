47ad1c8bbd45af7defc323743fd28f5c3fde8831
Morgan Hoban <mhhoban@gmail.com>
2017-03-03 21:40:14 -0800

    began implementing knockout js databindings

--- js/app.js
+++ js/app.js
@@ -1,10 +1,29 @@
 var map;
 
+function attraction(name, lat, lng) {
+  var self = this;
+  self.name = name;
+  self.lat = lat;
+  self.lng = lng;
+
+}
 
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -73.9980244},
-    zoom: 13
+    center: {lat: 37.811437, lng: -122.266787},
+    zoom: 16
   });
 }
+
+function appViewModel() {
+  var self = this;
+
+  self.attractions = ko.observableArray([
+    new attraction('Arbitrary place', 1, 2),
+    new attraction('Another Arbitrary Place', 3, 4)
+  ]);
+
+}
+
+ko.applyBindings(new appViewModel());
